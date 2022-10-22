0bbf2fa68b319655816091095becf2b16ed638d1
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2022-10-22 13:45:52 -0300

    commit inicial

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+package com.mycompany.helloword;
+
+/**
+ *
+ * @author User
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
